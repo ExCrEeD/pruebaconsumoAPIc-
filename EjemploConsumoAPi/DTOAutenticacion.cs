b48d6cd9aca2d0188b4839e4cc7fc18277771c96
ExCrEeD <daniel-alejandro_135@hotmail.com>
2019-12-04 14:32:08 -0500

    perimer commit

---
+++ EjemploConsumoAPi/DTOAutenticacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploConsumoAPi
+{
+    class DTOAutenticacion
+    {
+        public string NIT { get; set; }
+        public string usuario { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ EjemploConsumoAPi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+namespace EjemploConsumoAPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://www-prueba.titanio.com.co/PDE/public/api/auth/signin");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.UseDefaultCredentials = true;
+            httpWebRequest.PreAuthenticate = true;
+
+            DTOAutenticacion auth = new DTOAutenticacion()
+            {
+                NIT = "900103987",
+                usuario = "PHIPACKLTDA",
+                password = "qszLXoCo"
+            };
+            using (var streamWriter = new
+
+            StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = JsonConvert.SerializeObject(auth);
+                streamWriter.Write(json);
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.Write(result);
+            }
+
+        }
+    }
+}
